bc7659f96590adc4562793743dd7d051ecd03aeb
Dhaivat Patel <dhaivat.dhp@gmail.com>
2021-12-08 20:29:23 -0500

    updates README

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -5,87 +5,77 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
 
-	public float movementSpeed = 10f;
-	public float freeLookSensitivity = 3f;
-	public float zoomSensitivity = 10f;
-	/// Set to true when free looking (on right mouse button).
-	private bool looking = false;
+    public float msd = 10f;
+    public float fls = 3f;
+    public float zs = 10f;
+    private bool lk = false;
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.up * msd * Time.deltaTime);
 
-	// Start is called before the first frame update
-	void Start()
-	{
+        if (Input.GetKey(KeyCode.R))
+            Camera.main.transform.position = Camera.main.transform.position + (Vector3.up * msd * Time.deltaTime);
 
-	}
+        if (Input.GetKey(KeyCode.F))
+            Camera.main.transform.position = Camera.main.transform.position + (-Vector3.up * msd * Time.deltaTime);
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (Input.GetKey(KeyCode.A))
-			Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.right * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.forward * msd * Time.deltaTime);
 
-		if (Input.GetKey(KeyCode.D))
-			Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.right * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E))
+            Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.up * msd * Time.deltaTime);
 
-		if (Input.GetKey(KeyCode.W))
-			Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.forward * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.right * msd * Time.deltaTime);
 
-		if (Input.GetKey(KeyCode.S))
-			Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.forward * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.right * msd * Time.deltaTime);
 
-		if (Input.GetKey(KeyCode.E))
-			Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.up * movementSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            Camera.main.transform.position = Camera.main.transform.position + (Camera.main.transform.forward * msd * Time.deltaTime);
 
-		if (Input.GetKey(KeyCode.Q))
-			Camera.main.transform.position = Camera.main.transform.position + (-Camera.main.transform.up * movementSpeed * Time.deltaTime);
+        if (lk)
+        {
+            float nrx = Camera.main.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * fls;
+            float nry = Camera.main.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * fls;
+            Camera.main.transform.localEulerAngles = new Vector3(nry, nrx, 0f);
+        }
 
-		if (Input.GetKey(KeyCode.R))
-			Camera.main.transform.position = Camera.main.transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
+        float a = Input.GetAxis("Mouse ScrollWheel");
+        if (a != 0)
+        {
+            var zs = this.zs;
+            Camera.main.transform.position = Camera.main.transform.position + Camera.main.transform.forward * a * zs;
+        }
 
-		if (Input.GetKey(KeyCode.F))
-			Camera.main.transform.position = Camera.main.transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
 
-		if (looking)
-		{
-			float newRotationX = Camera.main.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
-			float newRotationY = Camera.main.transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
-			Camera.main.transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
-		}
+    void OnDisable()
+    {
+        StopLooking();
+    }
 
-		float axis = Input.GetAxis("Mouse ScrollWheel");
-		if (axis != 0)
-		{
-			var zoomSensitivity = this.zoomSensitivity;
-			Camera.main.transform.position = Camera.main.transform.position + Camera.main.transform.forward * axis * zoomSensitivity;
-		}
+    public void StartLooking()
+    {
+        lk = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
-		if (Input.GetKeyDown(KeyCode.Mouse1))
-		{
-			StartLooking();
-		}
-		else if (Input.GetKeyUp(KeyCode.Mouse1))
-		{
-			StopLooking();
-		}
-	}
-
-	void OnDisable()
-	{
-		StopLooking();
-	}
-
-	public void StartLooking()
-	{
-		looking = true;
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-	}
-
-	public void StopLooking()
-	{
-		looking = false;
-		Cursor.visible = true;
-		Cursor.lockState = CursorLockMode.None;
-	}
+    public void StopLooking()
+    {
+        lk = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
 }
-
